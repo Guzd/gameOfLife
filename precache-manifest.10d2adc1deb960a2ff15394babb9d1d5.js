f094901bb5c67bb9968d117a62b5985d0dd569b7
gutza <digutza.2@gmail.com>
2020-06-07 21:26:16 -0500

    Updates

---
+++ precache-manifest.10d2adc1deb960a2ff15394babb9d1d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dab3bcb07c27ede6fbaa959838054f2",
+    "url": "/gameOfLife/index.html"
+  },
+  {
+    "revision": "5be5f14f73c29c54cc62",
+    "url": "/gameOfLife/static/css/main.1e43c3da.chunk.css"
+  },
+  {
+    "revision": "394658a779c28a45f4b2",
+    "url": "/gameOfLife/static/js/2.96309521.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/gameOfLife/static/js/2.96309521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be5f14f73c29c54cc62",
+    "url": "/gameOfLife/static/js/main.dcc91700.chunk.js"
+  },
+  {
+    "revision": "8de0e517ec26c7fd5bbf",
+    "url": "/gameOfLife/static/js/runtime-main.a5276841.js"
+  }
+]);
